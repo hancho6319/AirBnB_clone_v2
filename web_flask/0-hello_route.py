bf620bcf4e3ef4e77d438e160ebe8215778eb9b5
hancho6319 <abbaauwalharuna@gmail.com>
2024-01-01 03:26:23 +0000

    GitHub repository: AirBnB_clone_v2     Directory: web_flask     File: 0-hello_route.py, __init__.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""Start a flask web app
+"""
 
-from web_flask import app
+from flask import Flask
+app = Flask(__name__)
 
-# Define a route for the root URL ('/')
-@app.route('/', strict_slashes=False)
-def hello():
+
+@app.route('/')
+def hello_flask():
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
