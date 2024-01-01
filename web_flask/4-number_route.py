e8fae27d2107d7668b4bb47f485825df04097d67
hancho6319 <abbaauwalharuna@gmail.com>
2024-01-01 13:07:20 +0000

    GitHub repository: AirBnB_clone_v2     Directory: web_flask     File: 3-python_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" web application to /: display “Hello HBNB!”,
+    /hbnb: display “HBNB”, and /c/<text>: display “C ”
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted with the replace
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """Return reformatted with the replaced
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/')
+@app.route('/number/<n>')
+def is_it_a_number(n:int):
+    """Return string when route queried
+    """
+    return 'n is a number'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
