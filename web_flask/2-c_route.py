dac2b77225e7c0249fa6a20e49c9c5bd2b221b19
hancho6319 <abbaauwalharuna@gmail.com>
2024-01-01 12:05:50 +0000

    GitHub repository: AirBnB_clone_v2     Directory: web_flask     File: 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start a flask web app
+""" web application to /: display “Hello HBNB!”,
+    /hbnb: display “HBNB”, and /c/<text>: display “C ”
 """
 
 
@@ -8,16 +9,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_flask():
+def hello():
     """Return string when route queried
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
     """Return string when route queried
     """
     return 'HBNB'
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted with the replace
+    """
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
